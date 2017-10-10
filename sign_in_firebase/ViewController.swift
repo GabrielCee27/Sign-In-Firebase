fcf5e1d908cc401dfd5c7093410d5edc3e3bfbc1
GabrielCee27 <gabrielcee81@gmail.com>
2017-10-10 16:40:34 -0700

    Initial Commit

---
+++ sign_in_firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sign_in_firebase
+//
+//  Created by Gabriel Cisneros on 10/10/17.
+//  Copyright © 2017 Gabriel Cisneros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
